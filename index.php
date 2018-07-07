a3f893acb28c7f3bf0b5359973d36c59872b642d
Carlos Sales <carlossalesti@gmail.com>
2018-07-07 19:51:12 -0300

    teste2

--- index.php
+++ index.php
@@ -1,33 +1,5 @@
 <?php
 
-$method = $_SERVER['REQUEST_METHOD'];
-
-if ($method == "POST"){
-    $requestBody = file_get_contents('php://input');
-    $json = json_decode($requestBody);
-    $text = $json->queryResult->parameters->Param;
-
-    switch($text){
-        case "001":
-            $speech = "Teste 01 - OK.";
-            break;
-        case "002":
-            $speech = "Teste 02 - OK.";
-            break;
-        default: 
-            $speech = "Teste não Especificado";
-            break;
-    }
-
-    $response = new \stdClass();
-    $response->speech = "";
-    $response->displayText = "";
-    $response->source = "webhook";
-    echo json_encode($response);
-
-
-else {
-    echo "method not allowed";
-}
+echo "Ok";
 
 ?>
